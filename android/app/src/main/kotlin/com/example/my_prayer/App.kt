902cb3dc4b4077d5785f2f0b4994be14dd55145a
Ali <daulay.alibana@gmail.com>
2025-02-10 11:19:54 +0700

    set workManager to update widgetApp android every 15 menit

--- android/app/src/main/kotlin/com/example/my_prayer/App.kt
+++ android/app/src/main/kotlin/com/example/my_prayer/App.kt
@@ -1,11 +1,27 @@
 package com.example.my_prayer
 
 import android.app.Application
-import com.orhanobut.hawk.Hawk
+import androidx.work.Configuration
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
+import java.util.concurrent.TimeUnit
 
 class App :Application() {
 
     override fun onCreate() {
         super.onCreate()
+        val workManagerConfig = Configuration.Builder().build()
+        WorkManager.initialize(this, workManagerConfig)
+
+        val periodicWorkRequest = PeriodicWorkRequest.Builder(
+            UpdateWidgetWorker::class.java, 15, TimeUnit.MINUTES
+        ).build()
+
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+            "PrayerWidgetUpdate",
+            ExistingPeriodicWorkPolicy.KEEP, // Prevents multiple instances
+            periodicWorkRequest
+        )
     }
 }
---
+++ android/app/src/main/kotlin/com/example/my_prayer/UpdateWidgetWorker.kt
@@ -0,0 +1,30 @@
+package com.example.my_prayer
+
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class UpdateWidgetWorker(context: Context, workerParameters: WorkerParameters) : Worker(context, workerParameters) {
+    override fun doWork(): Result {
+        Log.d("UpdateWidgetWorker", "Widget update task is running...")
+
+        try {
+            val intent = Intent(applicationContext, PrayerWidgetProvider::class.java).apply {
+                action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
+            }
+
+            applicationContext.sendBroadcast(intent)
+
+            Log.d("UpdateWidgetWorker", "Broadcast sent to update widget")
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return Result.failure()
+        }
+
+        return Result.success()
+    }
+}
